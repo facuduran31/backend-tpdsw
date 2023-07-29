692ddc2e85b95d2016e56939789e9fa0ddbc72a0
facu <facundoduran31@gmail.com>
2023-07-29 11:04:45 -0300

    Corregido el router login para que los parametros sean de req.body en lugar de req.params, añadido un archivo login.http para hacer test y verificar que login envía el token con exito

--- routers/login.js
+++ routers/login.js
@@ -4,27 +4,32 @@ const connection = require('../database/db');
 const SECRET_KEY = "claveSecreta";
 const routerLogin = express.Router();
 
-routerLogin.post('/:email/:password', (req, res) => 
-{
-    const usuarioAutenticado = null;
-    const email = req.params.email;
-    const password = req.params.password;
-    
-    // Verificar credenciales
-    connection.query('SELECT * FROM docentes WHERE email = ? AND password = ?', [email, password], (err, results) => {
-        if(err) throw res.status(500).send({error: 'Error'});
-        if(results.length === 0) {
-            return res.status(500).send({error: 'No se encontro ningun usuario con esas credenciales.'});
-        }
-        usuarioAutenticado = results[0];
-    });
+routerLogin.post('/', (req, res) => {
+  const { email, password } = req.body;
+
+  // Verificar credenciales
+  connection.query('SELECT * FROM docente WHERE email = ? AND password = ?', [email, password], (err, results) => {
+    if (err) {
+      console.error('Error al consultar la base de datos:', err);
+      return res.status(500).json({ error: 'Error al verificar las credenciales.' });
+    }
+
+    if (results.length === 0) {
+      return res.status(401).json({ error: 'No se encontró ningún usuario con esas credenciales.' });
+    }
+
+    const usuarioAutenticado = results[0];
 
     // Generar y enviar token
-    jwt.sign(usuarioAutenticado, SECRET_KEY, {expiresIn: '1h' }, (err, token) => {
-        if(err) throw res.status(500).json({error: 'Error al generar el token.'});
-        res.json({token});
-    });
-});
+    jwt.sign(usuarioAutenticado, SECRET_KEY, { expiresIn: '1h' }, (err, token) => {
+      if (err) {
+        console.error('Error al generar el token:', err);
+        return res.status(500).json({ error: 'Error al generar el token.' });
+      }
 
+      res.json({ token });
+    });
+  });
+});
 
 module.exports = routerLogin;
