77025a27250bfadf353e6e77341c8db1d0dae74b
facu <facundoduran31@gmail.com>
2023-08-31 20:13:08 -0300

    login para dos tipos de usuario

--- routers/login.js
+++ routers/login.js
@@ -7,7 +7,7 @@ const routerLogin = express.Router();
 routerLogin.post('/', (req, res) => {
   const { email, password } = req.body;
 
-  // Verificar credenciales
+  // Verificar credenciales y tipo de usuario
   connection.query('SELECT * FROM usuario WHERE email = ? AND password = ?', [email, password], (err, results) => {
     if (err) {
       console.error('Error al consultar la base de datos:', err);
@@ -34,27 +34,31 @@ routerLogin.post('/', (req, res) => {
 
 // Middleware para verificar el token en las rutas protegidas.
 function verificarToken(req, res, next) {
-    const token = req.header('Authorization');
-  
-    if (!token) {
-      return res.status(401).json({ error: 'Acceso denegado. Token no proporcionado.' });
-    }
-  
-    // Verificar el token con el método 'verify' de jsonwebtoken.
-    jwt.verify(token, SECRET_KEY, (err, decoded) => {
-      if (err) {
-        // El token no es válido.
-        console.error('Token inválido:', err);
-        return res.status(401).json({ error: 'Token inválido.' });
-      }
-  
-      // Token válido, puedes acceder a la información del usuario autenticado en 'decoded'.
-      req.usuarioAutenticado = decoded;
-      next();
-    });
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ error: 'Acceso denegado. Token no proporcionado.' });
   }
-  
+
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      // El token no es válido.
+      console.error('Token inválido:', err);
+      return res.status(401).json({ error: 'Token inválido.' });
+    }
+
+    // Verificar el tipo de usuario
+    const tipoUsuario = decoded.tipoUsuario;
+    if (tipoUsuario !== 'Docente' && tipoUsuario !== 'Encargado') {
+      return res.status(403).json({ error: 'Acceso denegado. Tipo de usuario no válido.' });
+    }
+
+    req.usuarioAutenticado = decoded;
+    next();
+  });
+}
+
 module.exports = {
-    routerLogin,
-    verificarToken
+  routerLogin,
+  verificarToken
 };
