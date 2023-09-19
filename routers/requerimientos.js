42fb672546f01d2d0218ce14681acd2e25fcf7d8
facu <facundoduran31@gmail.com>
2023-09-19 12:35:04 -0300

    Requerimientos

--- routers/requerimientos.js
+++ routers/requerimientos.js
@@ -39,7 +39,7 @@ routes.get('/', (req, res) => {
   
       conn.query(
           `UPDATE requerimiento SET tipoRequerimiento = ?, estado = ?, comentarioEncargado = ?, legajoEncargado = ?, legajoDocente = ?, hdmi = ?, vga = ?, mouse = ?, idLaboratorio = ?, fechaInicio = ?, fechaFin = ?, horaInicio = ?, horaFin = ?, materia = ?, zapatilla = ?, proyector = ?, tipoReserva = ?, descripcion = ? WHERE idRequerimiento = ?`,
-          [req.body.tipoRequerimiento, req.body.estado, req.body.programas, req.params.comentarioEncargado, req.params.legajoEncargado, req.params.legajoDocente, req.params.hdmi, req.params.vga, req.params.mouse, req.params.idLaboratorio, req.params.fechaInicio, req.params.fechaFin, req.params.horaInicio, req.params.horaFin, req.params.materia, req.params.zapatilla, req.params.proyector, req.params.tipoReserva, req.params.descripcion, req.params.idRequerimiento],
+          [req.body.tipoRequerimiento, req.body.estado, req.body.programas, req.body.comentarioEncargado, req.body.legajoEncargado, req.body.legajoDocente, req.body.hdmi, req.body.vga, req.body.mouse, req.body.idLaboratorio, req.body.fechaInicio, req.body.fechaFin, req.body.horaInicio, req.body.horaFin, req.body.materia, req.body.zapatilla, req.body.proyector, req.body.tipoReserva, req.body.descripcion, req.body.idRequerimiento],
           (err, result) => {
             if (err) return res.send(err);
             res.json(req.body);
@@ -48,22 +48,44 @@ routes.get('/', (req, res) => {
   });
   
   routes.post('/', (req, res) => {
-      const tipoUsuario = req.usuarioAutenticado.tipoUsuario;
-    
-      if (tipoUsuario !== 'Docente') {
-        return res.status(403).json({ error: 'Acceso denegado. Solo para Docentes.' });
-      }
+    const tipoUsuario = req.usuarioAutenticado.tipoUsuario;
   
-      conn.query(
-          `INSERT INTO requerimiento (tipoRequerimiento, estado, comentarioEncargado, legajoEncargado, legajoDocente, hdmi, vga, mouse, idLaboratorio, fechaInicio, fechaFin, horaInicio, horaFin, materia, zapatilla, proyector, tipoReserva, descripcion) 
-          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-          [req.body.tipoRequerimiento, req.body.estado, req.body.programas, req.body.comentarioEncargado, req.body.legajoEncargado, req.body.legajoDocente, req.body.hdmi, req.body.vga, req.body.mouse, req.body.idLaboratorio, req.body.fechaInicio, req.body.fechaFin, req.body.horaInicio, req.body.horaFin, req.body.materia, req.body.zapatilla, req.body.proyector, req.body.tipoReserva, req.body.descripcion, req.body.idRequerimiento],
-          (err, result) => {
-              if (err) return res.send(err);
-              res.json({ id: result.insertId });
-          }
-      );
+    if (tipoUsuario !== 'Docente') {
+      return res.status(403).json({ error: 'Acceso denegado. Solo para Docentes.' });
+    }
+  
+    console.log(req.body);
+  
+    conn.query(
+      `INSERT INTO requerimiento (tipoRequerimiento, estado, comentarioEncargado, legajoEncargado, legajoDocente, hdmi, vga, mouse, idLaboratorio, fechaInicio, fechaFin, horaInicio, horaFin, materia, zapatilla, proyector, tipoReserva, descripcion) 
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+      [
+        req.body.tipoRequerimiento,
+        req.body.estado,
+        req.body.comentarioEncargado,
+        req.body.legajoEncargado,
+        req.body.legajoDocente,
+        req.body.hdmi ? 1 : 0,
+        req.body.vga ? 1 : 0,
+        req.body.mouse ? 1 : 0,
+        req.body.idLaboratorio,
+        req.body.fechaInicio,
+        req.body.fechaFin,
+        req.body.horaInicio,
+        req.body.horaFin,
+        req.body.materia,
+        req.body.zapatilla ? 1 : 0,
+        req.body.proyector ? 1 : 0,
+        req.body.tipoReserva,
+        req.body.descripcion,
+      ],
+      (err, result) => {
+        if (err) return res.send(err);
+        res.json({ id: result.insertId });
+      }
+    );
   });
+  
   
   routes.delete('/:id', (req, res) => {
       const tipoUsuario = req.usuarioAutenticado.tipoUsuario;
