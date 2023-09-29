764e131330b33720a9e828d8a8e149ce63da1a92
Facundo Duran <facundoduran31@gmail.com>
2023-09-29 14:15:51 -0300

    Division en capas de computadoras

--- routers/computadorasRouter.js
+++ routers/computadorasRouter.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const ComputadorasController = require('../controllers/computadorasController');
 
 // Definir rutas
-router.get('/computadoras', ComputadorasController.ge);
-router.get('/computadoras/:id', ComputadorasController.getMaquinavirtualById);
-router.post('/computadoras', ComputadorasController.createMaquinavirtual);
-router.put('/computadoras/:id', ComputadorasController.updateMaquinavirtual);
-router.delete('/computadoras/:id', ComputadorasController.deleteMaquinavirtual);
+router.get('/computadoras', ComputadorasController.getAllComputadoras);
+router.get('/computadoras/:id', ComputadorasController.getComputadoraById);
+router.post('/computadoras', ComputadorasController.createComputadora);
+router.put('/computadoras/:id', ComputadorasController.updateComputadora);
+router.delete('/computadoras/:id', ComputadorasController.deleteComputadora);
 
 module.exports = router;
