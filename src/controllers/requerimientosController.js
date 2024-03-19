1457494775b83185549ae72cd2a1903adcd7c729
facu <facundoduran31@gmail.com>
2024-03-19 08:35:49 -0300

    requerimientos correcciones

--- src/controllers/requerimientosController.js
+++ src/controllers/requerimientosController.js
@@ -1,54 +1,66 @@
 const RequerimientoModel = require('../models/requerimientosModel');
 
 class RequerimientoController {
-  getAllRequerimientos(req, res){
-    RequerimientoModel.getAllRequerimientos((err, requerimientos) => {
-      if(err){
-        res.status(500).json({ error: 'Error al obtener los requerimientos', detalle: err });
-      } else {
-        res.json(requerimientos);
-      }
-    })
+
+  formatearFechas(requerimientos) {
+    requerimientos.forEach(requerimiento => {
+      requerimiento.fechaInicio = requerimiento.fechaInicio.toISOString().slice(0, 10);
+      requerimiento.fechaFin = requerimiento.fechaFin.toISOString().slice(0, 10);
+    });
   }
 
-  getRequerimientoById(req, res){
+  getAllRequerimientos(req, res) {
+    RequerimientoModel.getAllRequerimientos((err, requerimientos) => {
+      if (err) {
+        res.status(500).json({ error: 'Error al obtener los requerimientos', detalle: err });
+      } else {
+        this.formatearFechas(requerimientos);
+        res.json(requerimientos);
+      }
+    });
+  }
+
+  getRequerimientoById(req, res) {
     const id = req.params.id;
     RequerimientoModel.getRequerimientoById(id, (err, requerimiento) => {
-      if(err) {
+      if (err) {
         res.status(500).json({ error: 'Error al obtener el requerimiento' });
       } else {
+        this.formatearFechas([requerimiento]);
         res.json(requerimiento[0]);
       }
     });
   }
 
-  getRequerimientoByIdDocente(req, res){
+  getRequerimientoByIdDocente(req, res) {
     const id = req.params.id;
-    RequerimientoModel.getRequerimientoByIdDocente(id, (err, requerimiento) => {
-      if(err) {
+    RequerimientoModel.getRequerimientoByIdDocente(id, (err, requerimientos) => {
+      if (err) {
         res.status(500).json({ error: 'Error al obtener el requerimiento' });
       } else {
-        res.json(requerimiento);
+        this.formatearFechas(requerimientos);
+        res.json(requerimientos);
       }
     });
   }
 
-  createRequerimiento(req, res){
+  createRequerimiento(req, res) {
     const requerimiento = req.body;
     RequerimientoModel.createRequerimiento(requerimiento, (err, requerimiento) => {
-      if(err) {
-        res.status(500).json({ error: 'Error al crear el requerimiento'});
+      if (err) {
+        res.status(500).json({ error: 'Error al crear el requerimiento' });
       } else {
         res.json(requerimiento);
       }
     });
   }
 
-  updateRequerimiento(req, res){
+  updateRequerimiento(req, res) {
     const id = req.params.id;
     const requerimiento = req.body;
     RequerimientoModel.updateRequerimiento(id, requerimiento, (err, requerimiento) => {
-      if(err) {
+      if (err) {
+        console.log(err);
         res.status(500).json({ error: 'Error al actualizar el requerimiento', error: err });
       } else {
         res.json(requerimiento);
@@ -56,10 +68,10 @@ class RequerimientoController {
     });
   }
 
-  deleteRequerimiento(req, res){
+  deleteRequerimiento(req, res) {
     const id = req.params.id;
     RequerimientoModel.deleteRequerimiento(id, (err, requerimiento) => {
-      if(err) {
+      if (err) {
         res.status(500).json({ error: 'Error al eliminar el requerimiento' });
       } else {
         res.json(requerimiento);
